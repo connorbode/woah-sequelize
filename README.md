ba5dbf4181300d67ac523ee60616babf1d33849e
Connor Bode <connor.bode@gmail.com>
2015-08-20 21:43:12 -0400

    set up testing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # woah-sequelize
 Taming the beast.
+
+
+## Testing
+
+`npm test` to run tests.
+
+All test cases are written in `spec/spec.js`.
---
+++ spec/spec.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+beforeEach(done => {
+  done();
+});
+
+describe('test', () => {
+  it('tests', () => {
+    expect(true).to.equal(true);
+  });
+});
