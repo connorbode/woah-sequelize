a1f8dee5cb917ef5aea731ab606ea5d2d4e1a8cf
Connor Bode <connor.bode@gmail.com>
2015-08-21 20:55:58 -0400

    started wrapper

--- spec/spec.js
+++ spec/spec.js
@@ -2,9 +2,20 @@ var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 var should = chai.should();
+var woah = require('../lib');
+var models = require('./setup/models');
 
 beforeEach(done => {
+  var db = woah(models);
   done();
+});
+
+afterEach(done => {
+  models.Test
+    .destroy({ where: {} })
+    .then(() => {
+      done();
+    });
 });
 
 describe('test', () => {
